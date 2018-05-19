a0f120a30924e67f57c6b01c82460c2d646fff9b
Debuisson <jean.debuisson@decathlon.com>
2018-05-20 00:32:41 +0200

    add delete source file

--- src/main/java/com/supinfo/supdrive/controller/FileUploadController.java
+++ src/main/java/com/supinfo/supdrive/controller/FileUploadController.java
@@ -95,6 +95,7 @@ public class FileUploadController {
             byte[] bytes = file.getBytes();
             Path path = Paths.get(location + "/" +fileToUpload.getUuid());
             Files.write(path, bytes);
+            Files.delete(path);
         }
          catch (IOException e) {
         e.printStackTrace();
